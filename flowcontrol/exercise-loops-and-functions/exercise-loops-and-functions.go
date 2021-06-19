c45deea61af9d6182cdb400d74b429600fc8c252
Huy Vo <huy.vo@employmenthero.com>
2021-06-20 01:39:55 +0700

    Refactor code

--- flowcontrol/exercise-loops-and-functions/exercise-loops-and-functions.go
+++ flowcontrol/exercise-loops-and-functions/exercise-loops-and-functions.go
@@ -5,21 +5,18 @@ import (
 	"math"
 )
 
-const e = 1e-8
-
-func sqrt(x float64) float64 {
+func Sqrt(x float64) float64 {
 	z := 1.0
-
+	var t float64
 	for {
-		fmt.Println(z)
-		new_z := z - ((z*z - x) / (2 * z))
-		if math.Abs(new_z-z) < e {
-			return new_z
+		z, t = z-(z*z-x)/(2*z), z
+		if math.Abs(t-z) < 1e-6 {
+			break
 		}
-		z = new_z
 	}
+	return z
 }
 
 func main() {
-	fmt.Println(sqrt(2))
+	fmt.Println(Sqrt(2))
 }
